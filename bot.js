330bf1b2840f06dc4c2015de230f24445d825912
androsov-d <dmitriyandrosov0310@gmail.com>
2024-07-09 00:14:51 +0300

    implement 'add' command

--- bot.js
+++ bot.js
@@ -4,4 +4,10 @@ const grammy_1 = require("grammy");
 const bot = new grammy_1.Bot("7231883533:AAHTcdrpBJ4jSvDYgh7ujJbU1CylOgzG2Yk");
 bot.command("start", (ctx) => { var _a; return ctx.reply(`Hello, ${(_a = ctx.from) === null || _a === void 0 ? void 0 : _a.first_name}!`); });
 bot.command("like", (ctx) => ctx.react("👍"));
+bot.command("add", (ctx) => {
+    var _a;
+    const fullText = (_a = ctx.message) === null || _a === void 0 ? void 0 : _a.text;
+    const messageText = fullText === null || fullText === void 0 ? void 0 : fullText.split(" ").slice(1).join(" ");
+    ctx.reply(`You added ${messageText}`);
+});
 bot.start();
--- bot.ts
+++ bot.ts
@@ -4,5 +4,10 @@ const bot = new Bot("7231883533:AAHTcdrpBJ4jSvDYgh7ujJbU1CylOgzG2Yk");
 
 bot.command("start", (ctx) => ctx.reply(`Hello, ${ctx.from?.first_name}!`));
 bot.command("like", (ctx) => ctx.react("👍"));
+bot.command("add", (ctx) => {
+  const fullText = ctx.message?.text;
+  const messageText = fullText?.split(" ").slice(1).join(" ");
+  ctx.reply(`You added ${messageText}`);
+});
 
 bot.start();
