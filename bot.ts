fdb2679af61775027606ebcf1cd1e5ef2d47a9d3
androsov-d <dmitriyandrosov0310@gmail.com>
2024-07-08 22:53:40 +0300

    add greeting on start command

--- bot.ts
+++ bot.ts
@@ -1,3 +1,5 @@
 import { Bot } from "grammy";
 
 const bot = new Bot("7231883533:AAHTcdrpBJ4jSvDYgh7ujJbU1CylOgzG2Yk");
+
+bot.command("start", (ctx) => ctx.reply(`Hello, ${ctx.from?.first_name}!`));
