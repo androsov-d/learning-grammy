37ad9f06ca3fcadf5ee8b5931473a0a9519fead4
androsov-d <dmitriyandrosov0310@gmail.com>
2024-07-15 11:25:30 +0300

    add command menu

--- bot.js
+++ bot.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -17,17 +8,21 @@ const grammy_1 = require("grammy");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 function setCommands() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const commands = bot.api.getMyCommands();
-        bot.api.setMyCommands(yield commands);
-    });
+    bot.api.setMyCommands([
+        { command: "start", description: "Start the bot" },
+        { command: "like", description: "Add 'like' reaction on the message" },
+        { command: "add", description: "Add items and receive confirmation" },
+    ]);
 }
 const bot = new grammy_1.Bot(process.env.TELEGRAM_BOT_TOKEN);
-bot.command("start", (ctx) => { var _a; return ctx.reply(`Hello, ${(_a = ctx.from) === null || _a === void 0 ? void 0 : _a.first_name}!`); });
+bot.command("start", (ctx) => {
+    var _a;
+    ctx.reply(`Hello, ${(_a = ctx.from) === null || _a === void 0 ? void 0 : _a.first_name}!`);
+    setCommands();
+});
 bot.command("like", (ctx) => ctx.react("👍"));
 bot.command("add", (ctx) => ctx.reply(`You added ${(0, utils_1.getMessageText)(ctx)}`));
 bot.on("message", (ctx) => {
-    setCommands();
     const message = ctx.message.text;
     const filteredMessage = (0, utils_1.filterMessage)(message);
     if (filteredMessage !== "") {
--- bot.ts
+++ bot.ts
@@ -4,19 +4,24 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-async function setCommands() {
-  const commands = bot.api.getMyCommands();
-  bot.api.setMyCommands(await commands);
+function setCommands() {
+  bot.api.setMyCommands([
+    { command: "start", description: "Start the bot" },
+    { command: "like", description: "Add 'like' reaction on the message" },
+    { command: "add", description: "Add items and receive confirmation" },
+  ]);
 }
 
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN!);
 
-bot.command("start", (ctx) => ctx.reply(`Hello, ${ctx.from?.first_name}!`));
+bot.command("start", (ctx) => {
+  ctx.reply(`Hello, ${ctx.from?.first_name}!`);
+  setCommands();
+});
 bot.command("like", (ctx) => ctx.react("👍"));
 bot.command("add", (ctx) => ctx.reply(`You added ${getMessageText(ctx)}`));
 
 bot.on("message", (ctx) => {
-  setCommands();
   const message = ctx.message.text;
   const filteredMessage = filterMessage(message!);
   if (filteredMessage !== "") {
