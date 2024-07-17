65017293da8e03b9013974cb391ad34d0ee11f17
androsov-d <dmitriyandrosov0310@gmail.com>
2024-07-17 11:58:22 +0300

    add error catching

--- bot.ts
+++ bot.ts
@@ -1,5 +1,5 @@
 import { getMessageText, filterMessage } from "./utils";
-import { Api, Bot, Context } from "grammy";
+import { Api, Bot, Context, BotError, GrammyError, HttpError } from "grammy";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -26,6 +26,19 @@ bot.on("message", (ctx) => {
   const filteredMessage = filterMessage(message!);
   if (filteredMessage !== "") {
     ctx.reply(filteredMessage);
+  }
+});
+
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
   }
 });
 
